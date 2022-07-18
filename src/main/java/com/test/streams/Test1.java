b4f166e263dfcf49b277195399e64a01bf767a6c
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-07-18 22:50:33 +0530

    streams java class updated

--- src/main/java/com/test/streams/Test1.java
+++ src/main/java/com/test/streams/Test1.java
@@ -1,11 +1,16 @@
 package com.test.streams;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 {
+
 
 	// Count the number pf names starting with letter A in a list
 	// doing this the regular way
@@ -54,9 +59,36 @@ public class Test1 {
 		System.out.println(c);
 	}
 
-	@Test
+	//@Test
 	public void streamMap() {
+		ArrayList<String> names = new ArrayList<String>();
+
+		names.add("man");
+		names.add("don");
+		names.add("women");
+		
+		
 		Stream.of("Abhijeet", "Alekhya", "Agatha", "Rama", "Don").filter(s -> s.endsWith("a")).map(s -> s.toUpperCase())
 				.forEach(s -> System.out.println(s));
+		
+		List<String> names1 = Arrays.asList("Azbhijeet", "Don", "Alekhya", "Adam", "Rama");
+		names1.stream().filter(s-> s.startsWith("A")).sorted().map(s-> s.toUpperCase()).forEach(s->System.out.println(s));
+		
+		//merging two different lists
+		Stream<String> newStream = Stream.concat(names.stream(), names1.stream());
+		//newStream.sorted().forEach(s->System.out.println(s));
+		
+		boolean flag = newStream.anyMatch(s-> s.equalsIgnoreCase("Adam"));
+		System.out.println(flag);
+		Assert.assertTrue(flag);
+	}
+	
+	@Test
+	public void streamCollect() {
+		
+		List<String> ls = Stream.of("Abhijeet", "Alekhya", "Agatha", "Rama", "Don").filter(s -> s.endsWith("a")).map(s -> s.toUpperCase())
+		.collect(Collectors.toList());
+		
+		System.out.println(ls.get(0));
 	}
 }
